2bf9d4ee2422f139e2903c15c50d652458fb24d1
Yasuyuki ENDO <eyasuyuki@gmail.com>
2025-12-17 20:58:21 +0900

    initial commit

---
+++ render.sh
@@ -0,0 +1,19 @@
+ffmpeg -loop 1 -i slide_000.png -i 001_ずんだもん（ノーマル）_個人投資家が選ぶ\ ….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_000.mp4
+ffmpeg -loop 1 -i slide_001.png -i 002_ずんだもん（ノーマル）_投資家同士が\ 直接….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_001.mp4
+ffmpeg -loop 1 -i slide_002.png -i 003_ずんだもん（ノーマル）_そして.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_002.mp4
+ffmpeg -loop 1 -i slide_003.png -i 004_ずんだもん（ノーマル）_投資会社と\ 直接\ ….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_003.mp4
+ffmpeg -loop 1 -i slide_004.png -i 005_ずんだもん（ノーマル）_運用会社に意見を\ ….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_004.mp4
+ffmpeg -loop 1 -i slide_005.png -i 006_ずんだもん（ノーマル）_個人投資家の声が\ ….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_005.mp4
+ffmpeg -loop 1 -i slide_006.png -i 007_ずんだもん（ノーマル）_投資家が\ 主役の\ ….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_006.mp4
+ffmpeg -loop 1 -i slide_007.png -i 008_ずんだもん（ノーマル）_いつ？.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_007.mp4
+ffmpeg -loop 1 -i slide_008.png -i 009_ずんだもん（ノーマル）_2026年1月30….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_008.mp4
+ffmpeg -loop 1 -i slide_009.png -i 010_ずんだもん（ノーマル）_どこ？.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_009.mp4
+ffmpeg -loop 1 -i slide_010.png -i 011_ずんだもん（ノーマル）_東京茅場町　フィン….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_010.mp4
+ffmpeg -loop 1 -i slide_011.png -i 012_ずんだもん（ノーマル）_いくら？.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_011.mp4
+ffmpeg -loop 1 -i slide_012.png -i 013_ずんだもん（ノーマル）_一般来場　＋\ 懇親….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_012.mp4
+ffmpeg -loop 1 -i slide_013.png -i 014_ずんだもん（ノーマル）_さらに.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_013.mp4
+ffmpeg -loop 1 -i slide_014.png -i 015_ずんだもん（ノーマル）_トロフィー代\ への….wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_014.mp4
+ffmpeg -loop 1 -i slide_015.png -i 016_ずんだもん（ノーマル）_誰のため？.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_015.mp4
+ffmpeg -loop 1 -i slide_016.png -i 017_ずんだもん（ノーマル）_個人投資家\ のため.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_016.mp4
+ffmpeg -loop 1 -i slide_017.png -i 018_ずんだもん（ノーマル）_チケットは\ お早めに.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_017.mp4
+ffmpeg -loop 1 -i slide_018.png -i 019_ずんだもん（ノーマル）_購入はPeatix.wav  -r 30 -tune stillimage -fflags +genpts -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest slide_018.mp4
